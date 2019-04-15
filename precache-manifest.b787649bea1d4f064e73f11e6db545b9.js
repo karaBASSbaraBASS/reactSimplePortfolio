c3f9fe781dc7e93a1ae623a51ef166b9ce0030d1
Jack <evgen.mikhalov@gmail.com>
2019-04-16 00:29:29 +0300

    Updates

---
+++ precache-manifest.b787649bea1d4f064e73f11e6db545b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7530cb8848262622f7e4",
+    "url": "/reactSimplePortfolio/static/css/main.d9f83a2d.chunk.css"
+  },
+  {
+    "revision": "7530cb8848262622f7e4",
+    "url": "/reactSimplePortfolio/static/js/main.adcc08f0.chunk.js"
+  },
+  {
+    "revision": "f58a3f8b8b13b5a45cb6",
+    "url": "/reactSimplePortfolio/static/js/runtime~main.b03aeac4.js"
+  },
+  {
+    "revision": "e25fac56ec98f323bd9a",
+    "url": "/reactSimplePortfolio/static/js/2.90b9b39e.chunk.js"
+  },
+  {
+    "revision": "5279378f233a7ed513d5101fcacee947",
+    "url": "/reactSimplePortfolio/static/media/data-random-squares.5279378f.svg"
+  },
+  {
+    "revision": "740b51cb77dcf154c5c9c83d20707d40",
+    "url": "/reactSimplePortfolio/static/media/myface.740b51cb.png"
+  },
+  {
+    "revision": "5c2609f4ae29c92013a7461bf0c3144b",
+    "url": "/reactSimplePortfolio/index.html"
+  }
+];
