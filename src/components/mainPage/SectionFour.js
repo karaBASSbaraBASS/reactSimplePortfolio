30176ee2c3df5eacf432e0aa76b0da7cd8d4c2a6
Jack <evgen.mikhalov@gmail.com>
2019-05-02 20:30:16 +0300

    new text

--- src/components/mainPage/SectionFour.js
+++ src/components/mainPage/SectionFour.js
@@ -12,10 +12,9 @@ const SectionFour = () => {
                         </div>
                     </div>
                     <div className="textAbout">
-                        <p>I have a good knowledge in HTML5, CSS. Experienced Photoshop / Ilustrator user, work with *.psd/*.sketch layout. I can animate .svg graphics. Fiew years ago i had finished the system administration of local networks course.</p>
-                        <p>Since last 2 years I am actively engaged in HTML coding – HTML5, CSS. Have experience of Saas, MySQL, PHP and Java Script (ES5/ES6). In work I use "Chrom developer tools", "phpStorm", "VSCode". I know how to work with GIT, but prefer the bitbucket, because it helps to keep client privacy. A few years ago  worked with Jira and bug tracking Mantis.</p>
-                        <p>I love nature, mountain tourism and survival in the wild. Also like to cook interesting dishes and I go bouldering training one times of a week. I like to learn something new and finding the essence of the problem.</p>
-                        <p>In a future want to more practice in front-end development and I’ll do my best for it.</p>
+                        <p>I have a good knowledge in HTML5, CSS, Java Script (ES5/ES6). Experienced Photoshop / Ilustrator user, work with *.psd/*.sketch layout. I can animate .svg graphics. Fiew years ago i had finished the system administration of local networks course.</p>
+                        <p>Since last 2 years I am actively engaged in HTML coding – HTML5, CSS. Have experience of Saas, MySQL, PHP and Java Script (ES5/ES6). Now I spend a lot of time studying and writing applications on React. In work I use "Chrom developer tools", "phpStorm", "VSCode". I know how to work with GIT, but prefer the bitbucket, because it helps to keep client privacy. A few years ago  worked with Jira and bug tracking Mantis.</p>
+                        <p>I love nature, mountain tourism and go bouldering training one times of a week. Also like to cook interesting dishes. In a workflow like to learn something new and finding the essence of the problem. In a future want to more practice in front-end development and I’ll do my best for it.</p>
                     </div>
                 </div>
             </div>
